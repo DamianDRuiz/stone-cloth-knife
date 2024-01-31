cde3c8af20d07592dd89be581827623f25160a48
Damian <ruiz.damian17@gmail.com>
2024-01-31 15:26:47 -0500

    Routes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,16 +2,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../Pages/Home';
 import LogIn from '../Pages/login';
-import LogOut from '../Pages/logout';
 import SignUp from '../Pages/signup';
 
 export function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
+        <Route path="/stone-cloth-knife/" element={<Home />} />
+        <Route path="/stone-cloth-knife/signup" element={<SignUp />} />
+        <Route path="/stone-cloth-knife/login" element={<LogIn />} />
       </Routes>
     </>
   );
