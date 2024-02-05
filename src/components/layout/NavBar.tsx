b600a53a2e513a477aa0efa495a9ce2a687ac01e
Damian <ruiz.damian17@gmail.com>
2024-02-05 17:38:26 -0500

    NavBar

---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,31 @@
+import Userfront, { LogoutButton } from '@userfront/toolkit';
+import { Link } from 'react-router-dom';
+import styles from './NavBar.module.scss';
+
+const NavBar = ({ userData }) => {
+  const reload = () => setTimeout(() => document.location.reload(), 500);
+  const path = import.meta.env.VITE_PATH;
+  return (
+    <>
+      <div className={`${styles.greeting}`}>
+        Hi {userData.email ? userData.email : ''}{' '}
+      </div>
+      {Userfront.tokens.accessToken ? (
+        <div className={`logout ${styles.link}`} onClick={reload}>
+          <LogoutButton />
+        </div>
+      ) : (
+        <>
+          <div className={`${styles.link}`}>
+            <Link to={`${path}signup`}>Sign Up</Link>
+          </div>
+          <div className={`${styles.link}`}>
+            <Link to={`${path}login`}>Log In</Link>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default NavBar;
