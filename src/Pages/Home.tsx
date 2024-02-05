9ac13dd67455a88e4746fda31fd987eaa1986c76
Damian <ruiz.damian17@gmail.com>
2024-02-05 17:38:41 -0500

    Game

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,41 +1,103 @@
-import Userfront, { LogoutButton } from '@userfront/toolkit';
-import { Link, Navigate, useLocation } from 'react-router-dom';
+import Userfront from '@userfront/toolkit';
+import { useState } from 'react';
+import NavBar from 'src/components/layout/NavBar.tsx';
+
 const userData = Userfront.user;
 Userfront.init('vbq8rjzn');
 
-const reload = () => setTimeout(() => document.location.reload(), 500);
-
 const Home = () => {
-  const path = import.meta.env.VITE_PATH;
   return (
     <>
-      <div>Hi {userData.email ? userData.email : ''} </div>
-      {Userfront.tokens.accessToken ? (
-        <div className="logout" onClick={reload}>
-          <LogoutButton />
-        </div>
-      ) : (
-        <>
-          <div>
-            <Link to={`${path}signup`}>Sign Up</Link>
-          </div>
-          <div>
-            <Link to={`${path}login`}>Log In</Link>
-          </div>
-        </>
-      )}
+      <NavBar userData={userData} />
+      <Game />
     </>
   );
 };
 
-const RequireAuth = ({ children }: any) => {
-  let location = useLocation();
-  if (!Userfront.tokens.accessToken) {
-    // Redirect to the /login page
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
+const Game = () => {
+  const [p1Move, usep1Move] = useState<Move | null>(null);
+  const [p2Move, usep2Move] = useState<Move | null>(null);
+  const [turn, useTurn] = useState<Turn>('1');
 
-  return children;
+  const handleClick = (choice: Move) => {
+    if (turn == '1') {
+      usep1Move(choice);
+      useTurn('2');
+    } else if (turn == '2') {
+      usep2Move(choice);
+    }
+  };
+
+  const reset = () => {
+    usep1Move(null);
+    usep2Move(null);
+    useTurn('1');
+  };
+
+  const decideWinner = (player1Move: Move, player2Move: Move) => {
+    if (player1Move == null || player2Move == null) return;
+    const wins = { rock: 'scissors', paper: 'rock', scissors: 'paper' };
+
+    if (player1Move == player2Move) alert('stalemate');
+    else if (wins[player1Move] == player2Move) alert('1 wins');
+    else alert('2 wins');
+  };
+
+  return (
+    <>
+      <div>Player {`${turn}`}</div>
+      <MoveChoice
+        onClick={() => {
+          handleClick('rock');
+        }}
+        choice="rock"
+      />
+      <MoveChoice
+        onClick={() => {
+          handleClick('paper');
+        }}
+        choice="paper"
+      />
+      <MoveChoice
+        onClick={() => {
+          handleClick('scissors');
+        }}
+        choice="scissors"
+      />
+      <div>
+        <button
+          onClick={() => {
+            decideWinner(p1Move, p2Move);
+          }}
+        >
+          Reveal
+        </button>
+        <button onClick={reset}>New Game</button>
+      </div>
+    </>
+  );
 };
+
+const MoveChoice = ({ choice, onClick }: { choice: Move; onClick: any }) => {
+  return (
+    <>
+      <div onClick={onClick}>{choice != null ? choice : ''}</div>
+    </>
+  );
+};
+
+type Move = 'rock' | 'paper' | 'scissors';
+
+type Turn = '1' | '2';
+
+// const RequireAuth = ({ children }: any) => {
+//   let location = useLocation();
+//   if (!Userfront.tokens.accessToken) {
+//     // Redirect to the /login page
+//     return <Navigate to="/login" state={{ from: location }} replace />;
+//   }
+
+//   return children;
+// };
 
 export default Home;
